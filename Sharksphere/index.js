209b3fa5213a840ac912edd4f538069a7fd7ffd8
James Williams <40175864+Penguin3914@users.noreply.github.com>
2018-06-21 18:51:10 -0500

    Add files via upload

--- Sharksphere/index.js
+++ Sharksphere/index.js
@@ -1,8 +1,10 @@
+
 window.onload = function () {
     document.querySelector('.size007').onmouseover = btnSizeCarouselon;
     document.querySelector('.size007').onmouseout = btnSizeCarouseloff;
     document.querySelector('#toBe').onmouseover = toBeQuote;
     document.querySelector('#toBe').onmouseout = toBeQuotegone;
+    document.querySelector('#carder').onmouseover = cardBtnBorder
 }
 function btnSizeCarouselon() {
     this.innerHTML = "Learn more about Shakespere";
@@ -14,5 +16,8 @@ function toBeQuote(){
     this.innerHTML = "Prince Hamlet-Hamlet Act III, Scene I"
 }
 function toBeQuotegone(){
-    this.innerHTML = "To be or not to be, that is the question"
+    this.innerHTML = "To Be Or Not To be, That Is The Question."
+}
+function cardBtnBorder(){
+    this.style.border = "black"
 }
