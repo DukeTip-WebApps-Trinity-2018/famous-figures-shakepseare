7ea6c7cbf547f262c35b1e96431da639e74f2ddb
James Williams <40175864+Penguin3914@users.noreply.github.com>
2018-06-22 15:52:17 -0500

    Add files via upload

--- Sharksphere/index.js
+++ Sharksphere/index.js
@@ -1,23 +1,19 @@
+$(document).ready(function () {
+    $('.size007').hover(btnSizeCarouselon,btnSizeCarouseloff);
+    $('#toBe').hover(toBeQuote,toBeQuotegone);
+});
 
-window.onload = function () {
-    document.querySelector('.size007').onmouseover = btnSizeCarouselon;
-    document.querySelector('.size007').onmouseout = btnSizeCarouseloff;
-    document.querySelector('#toBe').onmouseover = toBeQuote;
-    document.querySelector('#toBe').onmouseout = toBeQuotegone;
-    document.querySelector('#carder').onmouseover = cardBtnBorder
+function btnSizeCarouselon(){
+    $(this).html("Learn More About Shakespeare");
+    
 }
-function btnSizeCarouselon() {
-    this.innerHTML = "Learn more about Shakespere";
-}
-function btnSizeCarouseloff(){
-    this.innerHTML = "William Shakespere"
-}
-function toBeQuote(){
-    this.innerHTML = "Prince Hamlet-Hamlet Act III, Scene I"
-}
-function toBeQuotegone(){
-    this.innerHTML = "To Be Or Not To be, That Is The Question."
-}
-function cardBtnBorder(){
-    this.style.border = "black"
-}
+function btnSizeCarouseloff() {
+  $(this).html("William Shakespeare")}
+
+
+  function toBeQuote() {
+     $(this).html("Prince Hamlet-Hamlet Act III, Scene I");
+ }
+ function toBeQuotegone() {
+     $(this).html("To Be Or Not To be, That Is The Question.")
+ }
